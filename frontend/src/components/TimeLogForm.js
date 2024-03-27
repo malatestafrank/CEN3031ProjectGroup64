93104ab70f5e9e143bb99f11e95bc76e3f3576ce
terrylu12212 <t.lu@ufl..edu>
2024-03-27 14:34:28 -0400

    Real-Time TimeLog Dropdown Updates

--- frontend/src/components/TimeLogForm.js
+++ frontend/src/components/TimeLogForm.js
@@ -1,54 +1,59 @@
 import { useState, useEffect } from "react"
+import { useProjectsContext } from "../hooks/useProjectsContext"
+import { useAuthContext } from "../hooks/useAuthContext"
 
 const TimeLogForm = () => {
-  const [selectedProject, setSelectedProject] = useState('')
-  const [projects, setProjects] = useState('')
+    const [selectedProject, setSelectedProject] = useState('')
+    const {projects, dispatch} = useProjectsContext()
+    const {user} = useAuthContext()
 
+    useEffect(() => {
+        const fetchProjects = async () => {
+            const response = await fetch('/api/projects', {
+                headers: {
+                    'Authorization': `Bearer ${user.token}`
+                }
+            })
+            const json = await response.json()
 
-  useEffect(() => {
-    const fetchProjects = async () => {
-        const response = await fetch('/api/projects')
-        const json = await response.json()
-
-        if(response.ok) {
-            setProjects(json)
+            if(response.ok) {
+                dispatch({type: 'SET_PROJECTS', payload: json})
+            }
         }
+        if(user) {
+            fetchProjects()
+        }
+    }, [dispatch, user]) //[] means the effect will only fire when the page is first loaded
+
+    const handleProjectSelection = (e) => {
+      setSelectedProject(e.target.value)
     }
 
-    fetchProjects()
-}, []) //[] means the effect will only fire when the page is first loaded
+    return (
+      
+      <form>
 
-
-  const handleProjectSelection = (e) => {
-    setSelectedProject(e.target.value)
-  }
-  return (
-
-   <form>
-
-    <h3>Time Logging Form</h3>
-    
-    <div>
-      <label>Select Project to Log Time</label>
-      <select id="project" value={selectedProject} onChange = {handleProjectSelection}>
-        <option>Select</option>
-        {projects && projects.map((project) => (
-      <option key={project._id}>{project.title}</option>
-    ))}
-      </select>
-      <p>You selected : {selectedProject}</p>
-    </div>
-
-    <label>Clock In Time: </label>
-    <input type='number'/>
-
-    <label>Clock Out Time: </label>
-    <input type='number'/>
-
-    <button type ='submit'>Submit Time</button> 
-   </form>
-  )
+      <h3>Time Logging Form</h3>
+      
+      <div>
+        <label>Select Project to Log Time</label>
+        <select id="project" value={selectedProject} onChange = {handleProjectSelection}>
+          <option>Select</option>
+          {projects && projects.map((project) => (
+        <option key={project._id}>{project.title}</option>
+      ))}
+        </select>
+      </div>
+  
+      <label>Clock In Time: </label>
+      <input type='number'/>
+  
+      <label>Clock Out Time: </label>
+      <input type='number'/>
+  
+      <button type ='submit'>Submit Time</button> 
+     </form>
+    )
 }
 
 export default TimeLogForm
-
