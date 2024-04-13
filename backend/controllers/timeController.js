433e1b9b01f116695a3a873296ab544ac6dca374
terrylu12212 <t.lu@ufl..edu>
2024-04-12 23:55:13 -0400

    FIxed Get and Delete Functions for Time Log

--- backend/controllers/timeController.js
+++ backend/controllers/timeController.js
@@ -1,9 +1,26 @@
 const TimeLog = require('../models/timelogModel')
+const mongoose = require('mongoose')
 
 //get all Time Logs
 const getTimeLogs = async (req, res) => {
     const logs = await TimeLog.find({})
     res.status(200).json(logs)
+}
+
+const getTimeLog = async (req, res) => {
+    const{id} = req.params
+
+    if(!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({error: 'Time Log Not Found'})
+    }
+
+    const timelog = await TimeLog.findById(id)
+
+    if(!timelog) {
+        return res.status(404).json({error: 'Time Log Not Found'})
+    }
+
+    res.status(200).json(timelog)
 }
 
 //create new Time Log
@@ -31,8 +48,10 @@ const deleteTimeLog = async (req, res) => {
     res.status(200).json(timelog)
 }
 
+
 module.exports = {
     getTimeLogs,
+    getTimeLog,
     createTimeLog,
     deleteTimeLog
 }
--- backend/routes/time.js
+++ backend/routes/time.js
@@ -1,12 +1,15 @@
 const express = require('express')
 
 //controller functions
-const {getTimeLogs, createTimeLog, deleteTimeLog} = require('../controllers/timeController')
+const {getTimeLogs, getTimeLog, createTimeLog, deleteTimeLog} = require('../controllers/timeController')
 
 const router = express.Router()
 
 
 router.get('/', getTimeLogs)
+
+//GET a single object
+router.get('/:id', getTimeLog)
 
 router.post('/', createTimeLog)
 
